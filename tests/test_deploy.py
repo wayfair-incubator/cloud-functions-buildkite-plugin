80d8ee12bfc9ff7dcf46cff8d9f80cf55c952abf
Jash Parekh <jparekh1@wayfair.com>
2021-12-14 14:42:19 -0500

    Lint

--- tests/test_deploy.py
+++ tests/test_deploy.py
@@ -4,8 +4,13 @@ from plugin_scripts import deploy
 
 
 @pytest.fixture
-def dataset_schema_directory(monkeypatch):
-    return monkeypatch.setenv("dataset_schema_directory", "schemas/project")
+def cloud_function_directory(monkeypatch):
+    return monkeypatch.setenv("cloud_function_directory", "schemas/project")
+
+
+@pytest.fixture
+def cloud_function_name(monkeypatch):
+    return monkeypatch.setenv("cloud_function_name", "cloud_function_name")
 
 
 @pytest.fixture
@@ -14,20 +19,25 @@ def gcp_project(monkeypatch):
 
 
 @pytest.fixture
+def gcp_region(monkeypatch):
+    return monkeypatch.setenv("gcp_region", "gcp_region")
+
+
+@pytest.fixture
 def credentials(monkeypatch):
     return monkeypatch.setenv("credentials", "{'secret': 'value'}")
 
 
-def test__validate_env_variables_missing_dataset_schema_directory(
-    gcp_project, credentials
+def test__validate_env_variables_missing_cloud_function_directory(
+    gcp_project, credentials, gcp_region, cloud_function_name
 ):
     with pytest.raises(Exception) as exec_info:
         deploy._validate_env_variables()
-    assert exec_info.value.args[0] == "Missing `dataset_schema_directory` config"
+    assert exec_info.value.args[0] == "Missing `cloud_function_directory` config"
 
 
 def test__validate_env_variables_missing_gcp_project(
-    dataset_schema_directory, credentials
+    cloud_function_directory, credentials, gcp_region, cloud_function_name
 ):
     with pytest.raises(Exception) as exec_info:
         deploy._validate_env_variables()
@@ -35,7 +45,7 @@ def test__validate_env_variables_missing_gcp_project(
 
 
 def test__validate_env_variables_missing_credentials(
-    gcp_project, dataset_schema_directory
+    gcp_project, cloud_function_directory, gcp_region, cloud_function_name
 ):
     with pytest.raises(Exception) as exec_info:
         deploy._validate_env_variables()
@@ -43,7 +53,7 @@ def test__validate_env_variables_missing_credentials(
 
 
 def test__validate_env_variables_all_variables_present(
-    gcp_project, dataset_schema_directory, credentials
+    gcp_project, cloud_function_directory, credentials, gcp_region, cloud_function_name
 ):
     deploy._validate_env_variables()
     assert True
